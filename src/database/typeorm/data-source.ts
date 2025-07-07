f43e0e4cfd27bdde6d635c0d0122125aab8fa9f0
Breno Nascimento <satsukebare23@gmail.com>
2025-07-07 14:56:44 -0300

    Alterando banco

--- src/database/typeorm/data-source.ts
+++ src/database/typeorm/data-source.ts
@@ -2,6 +2,19 @@ import { DataSource } from "typeorm";
 import path = require("path");
 import "dotenv/config";
 
+console.log({
+  type: "postgres",
+  host: process.env.DB_HOST || "localhost",
+  port: parseInt(process.env.DB_PORT || "5432"),
+  username: process.env.DB_USERNAME || "karate",
+  password: process.env.DB_PASSWORD || "postgres",
+  database: process.env.DB_NAME || "karate_db",
+  synchronize: false,
+  logging: true,
+  entities: [path.resolve(__dirname, "entity", "*{js,.ts}")],
+  migrations: [path.resolve(__dirname, "migrations", "*{js,.ts}")],
+});
+
 export const AppDataSource = new DataSource({
   type: "postgres",
   host: process.env.DB_HOST || "localhost",
@@ -13,5 +26,4 @@ export const AppDataSource = new DataSource({
   logging: true,
   entities: [path.resolve(__dirname, "entity", "*{js,.ts}")],
   migrations: [path.resolve(__dirname, "migrations", "*{js,.ts}")],
-  ssl: false,
 });
